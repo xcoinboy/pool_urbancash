71c2ba5cfc98f617b8c1abf15558de30e5f24989
xcoinboy <55258899+xcoinboy@users.noreply.github.com>
2021-08-26 21:23:30 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,13 @@
+var api = "https://centralpool.urbancash.org:8119";
+
+var email = "urbancash@urbancash.org";
+var telegram = "https://t.me/urbancash_official";
+var discord = "https://discord.gg/uYGAT6";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.urbancash.org/?hash={id}#";
+var transactionExplorer = "https://explorer.urbancash.org/?hash={id}#transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
